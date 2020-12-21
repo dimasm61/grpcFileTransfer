0872a40a2c16ac1751c9177a0258bc51b923cb05
Dimasm <dimasm_mail@mail.ru>
2020-12-21 11:11:25 +0300

    Update FileBufferItem.cs

--- solution/grpcFileTransfer.Server/Components/Buffer/FileBufferItem.cs
+++ solution/grpcFileTransfer.Server/Components/Buffer/FileBufferItem.cs
@@ -17,7 +17,6 @@ namespace grpcFileTransfer.Server.Components
         public void Dispose()
         {
             if (Data == null) return;
-            for (var i = 0; i < Data.Length; i++, Data[i] = 0) ;
             Data = null;
         }
 
